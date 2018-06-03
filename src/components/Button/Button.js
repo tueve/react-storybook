6b4d0fda6f2aaecef4527729e5d76e6819faec2e
Tue Vu <vu.tue.1710@gmail.com>
2018-06-03 16:02:25 +0700

    first structure for react-storybook

---
+++ src/components/Button/Button.js
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = ({
+  children,
+  className,
+  disabled,
+  small,
+  kind,
+  href,
+  tabIndex,
+  type,
+  icon,
+  iconDescription,
+  ...other
+}) => {
+  const buttonClasses = classNames(className, {
+    'bx--btn': true,
+    'bx--btn--sm': small,
+    'bx--btn--primary': kind === 'primary',
+    'bx--btn--danger': kind === 'danger',
+    'bx--btn--secondary': kind === 'secondary',
+    'bx--btn--ghost': kind === 'ghost',
+    'bx--btn--danger--primary': kind === 'danger--primary',
+    'bx--btn--tertiary': kind === 'tertiary'
+  });
+
+  const commonProps = {
+    tabIndex,
+    className: buttonClasses
+  };
+
+  const button = (
+    <button {...other} {...commonProps} disabled={disabled} type={type}>
+      {children}
+    </button>
+  );
+
+  const anchor = (
+    <a {...other} {...commonProps} href={href} role="button">
+      {children}
+    </a>
+  );
+
+  return href
+    ? anchor
+    : button;
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  small: PropTypes.bool,
+  kind: PropTypes
+    .oneOf([
+    'primary',
+    'secondary',
+    'danger',
+    'ghost',
+    'danger--primary',
+    'tertiary'
+  ])
+    .isRequired,
+  href: PropTypes.string,
+  tabIndex: PropTypes.number,
+  type: PropTypes.oneOf(['button', 'reset', 'submit']),
+  role: PropTypes.string,
+  icon: PropTypes.string,
+  iconDescription: props => {
+    if (props.icon && !props.iconDescription) {
+      return new Error('icon property specified without also providing an iconDescription property.');
+    }
+    return undefined;
+  }
+};
+
+Button.defaultProps = {
+  iconDescription: 'Provide icon description if icon is used',
+  tabIndex: 0,
+  type: 'button',
+  disabled: false,
+  small: false,
+  kind: 'primary'
+};
+
+export default Button;
